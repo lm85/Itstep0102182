ccd6f4bf43fc3419f1cbab5f1a319c7b0239a789
Milan Zlamal <milan.zlamal@homecredit.eu>
2017-08-08 19:53:49 +0200

    Merge origin/master

--- 13.6/trideniPole/trideniPole/src/tridenipole/TrideniPole.java
+++ 13.6/trideniPole/trideniPole/src/tridenipole/TrideniPole.java
@@ -7,14 +7,9 @@ package tridenipole;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.Scanner;
 
 /**
  *
