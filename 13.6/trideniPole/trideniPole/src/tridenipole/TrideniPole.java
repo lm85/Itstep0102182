2d9a2e18ac27dedd0aac30fe33889e56d85a1e82
Milan Zlamal <milan.zlamal@homecredit.eu>
2017-08-08 19:49:25 +0200

    Merge origin/master

--- 13.6/trideniPole/trideniPole/src/tridenipole/TrideniPole.java
+++ 13.6/trideniPole/trideniPole/src/tridenipole/TrideniPole.java
@@ -6,9 +6,13 @@
 package tridenipole;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -23,21 +27,48 @@ public class TrideniPole {
      */
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
+        String[] pole = new String[20];
+        int i = 0;
+        String radek = null;
 
         try (BufferedReader br = new BufferedReader(new FileReader("myNumbers.txt"))) {
             String s;
             while ((s = br.readLine()) != null) {
                 System.out.println(s);
+                pole[i] = s;
+                i++;
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("..\\..\\soubor1.csv"))) {
+            int[][] pole1 = new int[10][10];
+            for (int a = 0; a < 10; a++) {
+                for (int b = 0; b < 10; b++) {
+                    if (radek == null) {
+                        radek = "0";
+                    }
+                    if (a == 0 || b == 0) {
+                        a = a + 1;
+                        b = b + 1;
+                        pole1[a][b] = a + b;
+                    } else {
+                        pole1[a][b] = a * b;
+                    }
+                    radek += Integer.toString(pole1[a][b]) + ";";
+                    System.out.println(": :" + radek);
+                }
+
+                bw.write(radek);
+                bw.newLine();
+                radek = "";
             }
         } catch (Exception e) {
-            System.out.println("Chyba pri cteni ze souboru.");
+            System.out.println(e.getMessage());
         }
-/*
-        Arrays.sort(simpsonovi);
-        for (String s : simpsonovi) {
-            System.out.print(s + " ");
-        }
-*/
+
     }
 
 }
