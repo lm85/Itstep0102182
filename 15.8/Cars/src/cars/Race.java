5d21888f7ea1bf827ac760d0805ebc48f4f36dcd
Milan Zlamal <milan.zlamal@homecredit.eu>
2017-08-17 21:01:59 +0200

    Merge origin/master

---
+++ 15.8/Cars/src/cars/Race.java
@@ -0,0 +1,60 @@
+/*
+ * RAce - proste zavod aut
+ */
+package cars;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Milan Zlamal
+ */
+public class Race {
+
+    private int lenght = 0;
+    private ArrayList<RaceResults> results = new ArrayList<RaceResults>();
+    private ArrayList<Car> cars = new ArrayList<Car>();
+    private boolean ready = false;
+    
+    private int vyslednePrvni = 0;
+    
+    // vytvorim si zavod
+    public Race(int len) {
+        this.lenght = len;
+    }
+
+    // prihlasim do nej auta
+    public void add(Car car) {
+        this.cars.add(car);
+    }
+
+    // vytvorim start
+    public void start() {
+        this.ready = true;
+    }
+
+    // ziskani vysledku
+    private void getResult() {
+        if (!ready) {
+            return;
+        }
+    // Cas ujeti zavodu pro kazde auto
+    // vysledek seradi
+        
+    // pro celou kolekci cars - dle poctu v array listu
+        for (Car = tmpcar : cars) {  
+            results.add(
+                    new RaceResults(
+                            tmpcar, new TimeSpan(
+                                    this.lenght / tmpcar.getSpeed()
+                            )
+                    )
+            );
+        }
+        //dodelat
+        //results.sort(timeSpan);
+        
+        
+        
+    }
+}
--- 15.8/Cars/src/cars/RaceResults.java
+++ 15.8/Cars/src/cars/RaceResults.java
@@ -3,6 +3,8 @@
  */
 package cars;
 
+import java.util.Objects;
+
 /**
  *
  * @author Milan ZLamal
@@ -24,6 +26,32 @@ public class RaceResults {
 
     public Timespan getTimespan() {
         return this.timeSpan;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 47 * hash + Objects.hashCode(this.timeSpan);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        final RaceResults other = (RaceResults) obj;
+        
+        if (!Objects.equals(this.timeSpan, other.timeSpan)) {
+            return false;
+        }
+        return true;
+        
     }
 
 }
