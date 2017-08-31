7d7c2e919509b0de83b5259681d503cbd13b3a22
Milan Zlamal <milan.zlamal@homecredit.eu>
2017-08-31 19:01:24 +0200

    Merge origin/master

---
+++ rekaSpolecne/src/rekaspolecne/RekaSpolecne.java
@@ -0,0 +1,32 @@
+/*
+ * cilem je si hrat s lodicky
+ * vyrandomujeme si z lodi 2 tymy
+ * ty se budou strilet
+ * na zacatku hry se lode rozdeluji do tymu - pro zatim jen 2
+ * kazdej tym muze mit nejake zachranne lode
+ * kazdej tym ma svuj dock - lod v ni je docked - nemuze utocit a na ni nikdo neutoci
+ * kapacita docku je 1 lod
+ * porazena lod je sinked - stav
+ * kazda zachranna lod muze zachranit za hru jen jednu lod
+ * tuto lod pak dopravi do doku, jakmile je dock prazdny 
+ * - vybere hra tu co je zavesena do zachranare a co ma nejmene HP
+*/
+
+package rekaspolecne;
+
+/**
+ *
+ * @author Milan Zlamal
+ */
+public class RekaSpolecne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
