67bae562f186e1ac20980cf574fd96e6cbbf6849
Milan Zlamal <milan.zlamal@homecredit.eu>
2017-08-31 19:09:27 +0200

    Merge origin/master

--- rekaSpolecne/src/rekaspolecne/RekaSpolecne.java
+++ rekaSpolecne/src/rekaspolecne/RekaSpolecne.java
@@ -1,22 +1,13 @@
 /*
- * cilem je si hrat s lodicky
- * vyrandomujeme si z lodi 2 tymy
- * ty se budou strilet
- * na zacatku hry se lode rozdeluji do tymu - pro zatim jen 2
- * kazdej tym muze mit nejake zachranne lode
- * kazdej tym ma svuj dock - lod v ni je docked - nemuze utocit a na ni nikdo neutoci
- * kapacita docku je 1 lod
- * porazena lod je sinked - stav
- * kazda zachranna lod muze zachranit za hru jen jednu lod
- * tuto lod pak dopravi do doku, jakmile je dock prazdny 
- * - vybere hra tu co je zavesena do zachranare a co ma nejmene HP
-*/
-
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package rekaspolecne;
 
 /**
  *
- * @author Milan Zlamal
+ * @author Student
  */
 public class RekaSpolecne {
 
@@ -25,8 +16,6 @@ public class RekaSpolecne {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
     }
     
 }
