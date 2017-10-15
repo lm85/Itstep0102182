922d8c12b92217aa8c9c1406c0285aac2289e9fd
Necraatall <milan.zlamal.qa@gmail.com>
2017-10-15 15:30:16 +0200

    Merge origin/master

---
+++ 15.10/zakladyoop_rozsireni_trid/src/zakladyoop_rozsireni_trid/Zakladyoop_rozsireni_trid.java
@@ -0,0 +1,31 @@
+/*
+ * Upravte tridu Zvire() pro potreby dedeni
+ * pridejte metodu vydejZvuk() pro vydani zvuku zvirete
+ *  - metoda nemusi delat nic (vraci textovy retezec)
+ * ve tride definujte metodu toString pro jeji prevod na text
+ * Vytvorte nekolik trid konkretnich zvirat (kachna, pes, kocka)
+ *  - kazda trida pridava jeden atribut, ktery se inicializuje pres konstruktor
+ *  - kazde zvire upravuje metodu vydejZvuk() dle sveho
+ * Vytvorte pole typu Zvire a ulozit do nej nekolik instanci ruznych trid reprezentujicich zvirata
+ * Po te vypiste zvukyjednotlivych zvirat ulozenych v poli pomoci metody vydejZvuk()
+ * Nakonec vypiste instance zvirat v poli primo do textoveho vystupu
+ *
+ * 2* for each cyklus
+ */
+
+package zakladyoop_rozsireni_trid;
+
+/**
+ *
+ * @author Milan Z
+ */
+public class Zakladyoop_rozsireni_trid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ 15.10/zakladyoop_rozsireni_trid/src/zakladyoop_rozsireni_trid/Zvire.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package zakladyoop_rozsireni_trid;
+
+/**
+ *
+ * @author Milan Z
+ */
+public class Zvire {
+    
+}
