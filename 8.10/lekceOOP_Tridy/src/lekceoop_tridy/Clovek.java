4a2b303ae116d3090d5d2a96860c836ef90345e7
Necraatall <milan.zlamal.qa@gmail.com>
2017-10-15 12:13:16 +0200

    Merge origin/master

--- 8.10/lekceOOP_Tridy/src/lekceoop_tridy/Clovek.java
+++ 8.10/lekceOOP_Tridy/src/lekceoop_tridy/Clovek.java
@@ -1,17 +1,15 @@
 package lekceoop_tridy;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
  */
 
 /**
  *
- * @author ASUS
+ * @author Milan Z
  */
 public class Clovek {
-    public String jmeno;
+    private String jmeno;
     private int vek = 42;
 
     // bez static je metoda dane tridy se static je obecna funkce
@@ -29,30 +27,68 @@ public class Clovek {
         // vypis s formatovanym stringem
         System.out.printf("Ahoj %s, %d,", jmeno, vek);
     }
+
     /* potrebuji zaokrouhlovani - double a float
     udelam obe 2 a pak si s ni pohraji
     pretizena metoda - pokud mam metodu stejne jmenujici se je pretizena
     proste si ji definuji nekolikrat s parametrama a pak si ji zavolam 
     na potrebne fungovani
-    
     pretezovani:
-    ***
+     ***
     bez atributu
-        public void pozdrav (){
-        
-    // lepe delegovat - proste napsat s volanim 
-        // vypis s formatovanym stringem
-        System.out.printf("Ahoj %s, %d,", pozdrav, jmeno, vek);
+    public void pozdrav (){
+    // lepe delegovat - proste napsat s volanim
+    // vypis s formatovanym stringem
+    System.out.printf("Ahoj %s, %d,", pozdrav, jmeno, vek);
+    }
+    bez atributu
+    public void pozdrav (String pozdrav){
+    // lepe delegovat - proste napsat s volanim
+    // vypis s formatovanym stringem
+    System.out.printf("Ahoj %s, %d,", pozdrav, jmeno, vek);
+    }
+     ***
+     */
+    /*
+    // konstruktor
+    public Clovek(String jmeno, int vek){
+    // this pouzijeme pro rozliseni atributu a parametru metody
+    this.jmeno = jmeno;
+    this.vek= vek;
+    }
+     */
+    
+    // Metody pro pozdrav ponechame
+    
+    /*
+    setter generujeme alt+insert - nastavime hodnotu 
+        pro ziskani hodnoty z private promennych (atributu)
+        - prepise se puvodni pokud nejaka je
+    */
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
     }
     
-    bez atributu
-        public void pozdrav (String pozdrav){
-        
-    // lepe delegovat - proste napsat s volanim 
-        // vypis s formatovanym stringem
-        System.out.printf("Ahoj %s, %d,", pozdrav, jmeno, vek);
-    }
-    ***
+    /*
+    setter generujeme alt+insert - nastavime hodnotu 
+        pro ziskani hodnoty z private promennych (atributu)
+        - prepise se puvodni pokud nejaka je
     */
+    public void setVek(int vek) {
+        this.vek = vek;
+    }
+
+    /*
+    getter generujeme alt+insert - vememe si hodnotu 
+        pro ziskani hodnoty z private promennych (atributu)
+        - vememe prepsanou hodnotu promenne - atributu settrem
+    */
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public int getVek() {
+        return vek;
+    }
     
 }
