34a6e958236bd2b5aec0d6293704f074bf4e7516
Necraatall <milan.zlamal.qa@gmail.com>
2017-10-15 10:23:56 +0200

    Merge origin/master

---
+++ 8.10/lekceOOP_Tridy/src/lekceoop_tridy/LekceOOP_Tridy.java
@@ -0,0 +1,28 @@
+/*
+ * toto je na nauceni tvorby tridy a prace s nimi
+ */
+
+package lekceoop_tridy;
+
+/**
+ *
+ * @author Milan Z
+ */
+public class LekceOOP_Tridy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        //vytvoreni instance - vytvarim noveho cloveka
+        Clovek karel = new Clovek();
+        karel.jmeno = "Karel";
+    
+        //Tohle nejde! Proč? -- vek je privatni pro tridu Clovek
+        //karel.vek = 42;
+    
+    }
+    
+}
