d878321b5ca17e5eb40a88e7aa89ab7f4bc6d35c
Necraatall <milan.zlamal.qa@gmail.com>
2017-11-26 10:27:33 +0100

    Revert "Merge origin/master"
    This reverts commit 68d71e7f71ac1dce54dd6b319343f4b749f817fc.

---
+++ 22.8-doma/rozcvicka/src/rozcvicka/Rozcvicka.java
@@ -0,0 +1,30 @@
+/*
+ * 1. fce ktera si bere parametry a, b, c a vypise cisla intervalu a az b
+ * delitelna cislem c
+ * napr: Input(a=5, b=12, c=3)
+ * Output: 6, 9, 12
+ * 2. fce ktera si bere parametry a, b, a vypise prvocisla v intervalu a az b
+ * napr:
+ * input: (5,13)
+ * Output: 5, 7, 11, 13
+ * dodelat doma
+ */
+
+package rozcvicka;
+
+/**
+ *
+ * @author Milan Zlamal
+ */
+public class Rozcvicka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
